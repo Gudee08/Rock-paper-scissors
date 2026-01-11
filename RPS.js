5be16a3a9388d18d034c94bd1d25257510cf1e3c
Gudee08 <160516062+Gudee08@users.noreply.github.com>
2026-01-11 23:49:51 +0200

    modified

--- RPS.js
+++ RPS.js
@@ -12,3 +12,32 @@ function getcomputerchoice() {
   }
 }
 console.log(getcomputerchoice());
+
+function gethumancchoice() {
+  const humanchoice = prompt("rock, paper, or scissors");
+  return humanchoice;
+}
+console.log(gethumancchoice());
+
+let computerchoice = 0;
+let humanchoice = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("it is a tie!");
+    return;
+  }
+  if (humanChoice === "rock" && computerChoice === "scissors")
+    //
+    (humanChoice === "paper" && computerChoice === "rock")(
+      //
+      humanchoice === "scissors" && computerChoice === "paper"
+    );
+  {
+    humanscore++;
+    console.log("you won!) ${humanchoice} beats ${computerChoice}");
+  }
+  computerChoice++;
+  console.log("you lost! ${computerChoice} beats ${humanChoice");
+}
