62f8b7597f91681fbada6c99441da2a1f48e300b
Gudee08 <160516062+Gudee08@users.noreply.github.com>
2026-01-13 22:09:00 +0200

    game completed, but still needs fixing

--- RPS.js
+++ RPS.js
@@ -41,3 +41,34 @@ function playRound(humanChoice, computerChoice) {
   computerChoice++;
   console.log("you lost! ${computerChoice} beats ${humanChoice");
 }
+
+function playgame() {
+  let computerchoice = 0;
+  let humanChoice = 0;
+  let round = 1;
+  round <= 5;
+  console.log(round);
+
+  const result = playRound();
+  if (round === "computerChoice") {
+    computerchoice++;
+    console.log("computer won");
+  } else if (round === "humanChoice") {
+    humanChoice++;
+    console.log("you won!");
+  } else {
+    console.log("this round is a tie!");
+  }
+  round++;
+}
+console.log("final score");
+console.log("human", humanchoice);
+console.log("computer", computerchoice);
+
+if (humanchoice > computerchoice) {
+  return "you won the game!";
+} else if (computerchoice > humanchoice) {
+  return "computer won the game!";
+} else {
+  return "the game is a tie!";
+}
